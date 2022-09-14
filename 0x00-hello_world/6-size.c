71e4aeba0366edc9f8f2094b308461f59fa1fe9f
Mumbe-data <mumbekitonga@gmail.com>
2022-09-14 04:00:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print string in the put function
  *
  * Description: using the main function
- * this program prints the size of various types on the computer it is compiled and run on
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
